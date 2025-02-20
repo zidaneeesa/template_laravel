da782292d4ea679990f3ac433884e91b3df65840
zidaneeesa <zidanejulian7265@gmail.com>
2025-02-20 23:49:14 +0700

    Initial commit

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Selamat Datang di Home</h1>
+    <p>Ini adalah halaman utama setelah login.</p>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TemplateController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/main', function () {
+    return view('layouts.main');
+});
+
+// Route::get('/home', [TemplateController::class, 'index']); // Fixed capitalization of 'Route'
+
+Route::get('/dashboard', function () {
+    return view('layouts.main');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
